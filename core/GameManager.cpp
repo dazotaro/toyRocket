7971e5f7fb64f7d379199abba89dbaf6d6a21797
dazotaro <dazotaro@gmail.com>
2015-06-30 01:01:55 +0200

    Calculate Frame rate

--- core/GameManager.cpp
+++ core/GameManager.cpp
@@ -7,7 +7,7 @@
 
 // Local includes
 #include "GameManager.hpp"
-
+#include "Timer.hpp"		// JU::Timer
 // Global includes
 #include <cstdio>       // std::printf
 
@@ -55,8 +55,30 @@ bool GameManager::initialize()
 
 void GameManager::loop()
 {
+	// FPS Calculation
+	uint32 counted_frames = 0;
+	Timer timer;
+	timer.start();
+
 	while(running_)
 	{
+		uint32 time = timer.getTicks();
+		if (time > 1000)
+		{
+			//Calculate and correct fps
+			float avgFPS = counted_frames  * 1000.0f / time;
+			if( avgFPS > 2000000 )
+			{
+				avgFPS = 0;
+			}
+
+			std::printf("FPS = %f\n", avgFPS);
+			timer.start();
+			counted_frames = 0;
+		}
+		else
+			counted_frames++;
+
 		event_manager_.update();
 		if (event_manager_.quitting())
 		{
@@ -66,8 +88,6 @@ void GameManager::loop()
 		state_manager_.update();
 		state_manager_.draw();
 		window_.render();
-
-
 	}
 	state_manager_.exit();
 }
